97dafb018d690fe8693b88cb04ee09c3b50eb032
abc <abc@DESKTOP-ALKHQS4>
2022-02-12 20:15:29 -0800

    project

---
+++ eve1/src/eve1/add.java
@@ -0,0 +1,17 @@
+package eve1;
+
+public class add {
+
+	public static void main(String[] args) {
+		int f=100;
+	    int s=20;
+	    System.out.println(f + " " + s);
+	    int sum=f+s;
+	    System.out.println("the sum is:" +sum);
+	
+
+	    
+
+	}
+
+}
